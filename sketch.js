bf5a5ac7bb9117c1ceb66e02c836b8a817b59c73
giuliagal93 <peke@MacBook-Air.local>
2016-12-11 13:21:25 +0100

    09

--- sketch.js
+++ sketch.js
@@ -1,18 +1,43 @@
 var x = 20;
+var posy = 100;
+
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     noStroke();
     frameRate(10);
 
+
+
+
 } 
 
-var posy = 100;
+var value = 0
+
+function deviceTurned() {
+  if (value == 0){
+    value = 255
+  } else if (value == 255) {
+    value = 0;
+  }
+}
+
+function deviceShaken() {
+  posy = posy + 1;
+  if (posy > height-100) {
+    posy = 100;
+  }
+}
 
 function draw() {
     
-    background(200,100,255,70);
+   background(100,100,255,70);
+        
+    fill(value);
+    ellipse(width/2, posy, 200, 200);
+
     
-    if(touchIsDown){
+   /* if(touchIsDown){
     
     var colorList = ['#eeeeee',
                  '#333333',
@@ -34,50 +59,15 @@ function draw() {
         
     for(index=0; index < colorList.length; index++){
     
-    /*y = x - 50;
-    x = y;*/
+    y = x - 50;
+    x = y;
         
     fill(colorList[index]);
 
     ellipse(width/2, posy, 200-40*index);
 
-    }
+    }*/
     
-
-    /*
-    
-    fill(colorList[0]);
-    ellipse(width/2, height/2, 400,400)
-    
-    
-    
-    
-    if(touchIsDown) {
-        
-        for(i=0; i < 1 ; i++) {
-         var y = x + 1;
-         x = y;
-        };
-        
-    } else if(touchIsReleased){
-          for(i=0; i < 1 ; i++) {
-         var y = x - 1;
-         x = y;
-        };
-        };
-
-        
-            
-    noStroke;
-    fill(220);
-               rect(0,0,windowWidth,x);
-*/
   
 }
 
-function deviceShaken() {
-  posy = posy + 1;
-  if (posy > height-100) {
-    posy = 100;
-  }
-}
